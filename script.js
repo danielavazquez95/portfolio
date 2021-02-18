ae3bccae05f38d4567cfb87b7df511f4e29b4b86
Daniela Vazquez <danielavazquez95@gmail.com>
2021-02-18 20:02:13 -0300

    background refactoring

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["React JS Front End Developer"],
+        strings: ["Front End Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
